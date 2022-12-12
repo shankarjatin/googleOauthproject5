5fcab2e32c01dc6528421ef0e28b0ca25977524b
shankarjatin <shankarjatin1005@gmail.com>
2022-12-12 20:48:55 +0530

    google credential edit

--- passport-setup.js
+++ passport-setup.js
@@ -22,7 +22,7 @@ passport.deserializeUser(function(user, done) {
 passport.use(new GoogleStrategy({
     clientID:process.env.GOOGLE_CLIENT_ID,
     clientSecret:process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL:"http://localhost:3000/google/callback",
+    callbackURL:"https://hashes-registration-jatin.onrender.com/google/callback",
     passReqToCallback:true
   },
   function(request, accessToken, refreshToken, profile, done, ) {
